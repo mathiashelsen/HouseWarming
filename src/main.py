9d7d55d9de4c8ce759243674865e7732f872d54d
Mathias Helsen <helsen.mathias@gmail.com>
2016-12-13 17:49:41 +0100

    first settings import created

--- src/main.py
+++ src/main.py
@@ -8,20 +8,31 @@ errLog = file('/home/pi/houseWarming.log', 'a')
 
 defTemp = 10.0
 hyst = 0.5
+calendar = dict()
 
 errLog.write('# Script started execution\n')
 errLog.write('# ' + str(datetime.today()) + '\n')
 errLog.flush()
 
 print settings.shape
-if(settings.shape[1] > 2):
+if(settings.shape[0] > 2):
     defTemp = settings[0,0]
     hyst = settings[1,0]
+    for i in range(2,settings.shape[0]):
+	day = settings[i,0]
+	start = settings[i,1]
+	stop = settings[i,2]
+	setpoint = settings[i,3]
+	if day in calendar.keys():
+	    calendar[day].append([start, stop, setpoint])
+	else:
+	    calendar[day] = [[start, stop, setpoint]]
 else:
     errLog.write('# Insufficient settings provided\n')
     errLog.flush()
 
 print defTemp, hyst
+print calendar
 
 errLog.flush()
 errLog.close()
