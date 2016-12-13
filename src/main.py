03bdb3bb06b571237e156bf448abd235d553388b
Mathias Helsen <helsen.mathias@gmail.com>
2016-12-13 17:38:35 +0100

    fixed path error for settings

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 import numpy as np
 from datetime import datetime
 
-settings = np.genfromtxt('/home/pi/HouseWarming/example/normal.csv', delimiter=';')
+settings = np.genfromtxt('/home/pi/HouseWarming/examples/normal.csv', delimiter=';')
 errLog = file('/home/pi/houseWarming.log', 'a')
 
 defTemp = 10.0
