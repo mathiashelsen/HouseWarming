9f6747add081372951688ed26cdebb8ad3b2f60d
Mathias Helsen <helsen.mathias@gmail.com>
2016-12-13 19:22:11 +0100

    finished first rough draft

--- src/main.py
+++ src/main.py
@@ -6,9 +6,13 @@ from datetime import datetime
 settings = np.genfromtxt('/home/pi/HouseWarming/examples/normal.csv', delimiter=';')
 errLog = file('/home/pi/houseWarming.log', 'a')
 
-defTemp = 10.0
-hyst = 0.5
+defaultTemp = 10.0
+hysteresis = 0.5
 calendar = dict()
+
+currentTemp = 21.0
+targetTemp = defTemp
+statusCV = False
 
 errLog.write('# Script started execution\n')
 errLog.write('# ' + str(datetime.today()) + '\n')
@@ -16,8 +20,8 @@ errLog.flush()
 
 print settings.shape
 if(settings.shape[0] > 2):
-    defTemp = settings[0,0]
-    hyst = settings[1,0]
+    defaultTemp = settings[0,0]
+    hysteris = settings[1,0]
     for i in range(2,settings.shape[0]):
 	day = settings[i,0]
 	start = settings[i,1]
@@ -33,6 +37,37 @@ else:
 
 print defTemp, hyst
 print calendar
+
+while(True):
+    # Set default temperature as fallback
+    if(statusCV):
+	targetTemp = defaultTemp + hysteresis
+    else
+	targetTemp = defaultTemp - hysteresis
+
+    # Look up the settings for today
+    if datetime.today().weekday() in calendar.keys():
+	todaySettings = calendar[datetime.today().weekday()]
+	time = datetime.today().hour + datetime.today().minute/60.0
+
+	# Find the slot in which we are
+	for i in range(len(todaySettings)):
+	    if( time >= todaySettings[i][0] and time < todaySettings[i][1]):
+		if(statusCV):
+		    targetTemp = todaySettings[i][2] + hysteresis
+		else(statusCV):
+		    targetTemp = todaySettings[i][2] - hysteresis
+
+	print todaySettings
+	print targetTemp
+
+    # Apply control to the CV system
+    if( currentTemp > targetTemp ):
+	statusCV = False
+	# CV Off
+    else:
+	statusCV = True
+	# CV On
 
 errLog.flush()
 errLog.close()
