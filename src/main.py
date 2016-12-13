ede88eb0929f2d251b30b15a563da618c930ddfa
Mathias Helsen <helsen.mathias@gmail.com>
2016-12-13 17:36:36 +0100

    added datetime basic check

--- src/main.py
+++ src/main.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python2.7
 
 import numpy as np
-from datetime import date
+from datetime import datetime
 
-settings = np.genfromtxt('../examples/normal.csv', delimiter=';')
-errLog = file('/home/pi/houseWarming.err', 'a')
+settings = np.genfromtxt('/home/pi/HouseWarming/normal.csv', delimiter=';')
+errLog = file('/home/pi/houseWarming.log', 'a')
 
 defTemp = 10.0
 hyst = 0.5
 
-print date.today
-
-try:
-    # Try to construct the dictionary
-    if(settings.shape[1] < 3):
-	print "not enough settings provided"
-    for i in range(settings.shape[1]-2):
-	    
+errLog.write('# ' + datetime.today() + '\n')
+errLog.flush()
