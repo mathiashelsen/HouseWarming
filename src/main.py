ba653dcb5f504fdbaeb662a4f92cd1d63159328a
Mathias Helsen <helsen.mathias@gmail.com>
2016-12-13 17:31:04 +0100

    reading the xample file and printing it

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python2.7
 
 import numpy as np
+from datetime import date
 
 settings = np.genfromtxt('../examples/normal.csv', delimiter=';')
-print settings
+errLog = file('/home/pi/houseWarming.err', 'a')
+
+defTemp = 10.0
+hyst = 0.5
+
+print date.today
+
+try:
+    # Try to construct the dictionary
+    if(settings.shape[1] < 3):
+	print "not enough settings provided"
+    for i in range(settings.shape[1]-2):
+	    
