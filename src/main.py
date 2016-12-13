14658af8e9eedca396921f205904ed5c20807965
Mathias Helsen <helsen.mathias@gmail.com>
2016-12-13 17:42:15 +0100

    first settings import created

--- src/main.py
+++ src/main.py
@@ -9,5 +9,18 @@ errLog = file('/home/pi/houseWarming.log', 'a')
 defTemp = 10.0
 hyst = 0.5
 
+errLog.write('# Script started execution\n')
 errLog.write('# ' + str(datetime.today()) + '\n')
 errLog.flush()
+
+if(settings.shape[1] > 2):
+    defTemp = settings[0,0]
+    hyst = settings[1,0]
+else:
+    errLog.write('# Insufficient settings provided\n')
+    errLog.flush()
+
+print defTemp, hyst
+
+errLog.flush()
+errLog.close()
